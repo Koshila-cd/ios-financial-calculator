e4f7ff7610f6bccb9a05c43523d10f3e9058cfde
Koshila Dissanayake <koshila.2016540@iit.ac.lk>
2020-03-14 16:17:10 +0530

    Help view

--- ios-financial-calculator/Controller/HelpViewController.swift
+++ ios-financial-calculator/Controller/HelpViewController.swift
@@ -8,14 +8,50 @@
 
 import UIKit
 
-class HelpViewController: UIViewController {
+class HelpViewController: UIViewController, UIScrollViewDelegate {
 
+    @IBOutlet weak var scrollView: UIScrollView!
+    @IBOutlet weak var pageControl: UIPageControl!
+    
+    var helpScreens: [String] = ["help-1","help-2","help-3"]
+    var frame = CGRect.zero
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        self.view.applyGradient(colours: [.black, .purple, .darkGray], locations: [0.0, 0.5, 1.0])
+        
+        pageControl.numberOfPages = helpScreens.count
+        setupScreens()
+        
+        scrollView.delegate = self
+        
     }
     
-    @IBOutlet weak var helpView: UIView!
+    
+    func setupScreens() {
+        for index in 0..<helpScreens.count {
+            // 1.
+            frame.origin.x = scrollView.frame.size.width * CGFloat(index)
+            frame.size = scrollView.frame.size
+            
+            // 2.
+            let imgView = UIImageView(frame: frame)
+            imgView.image = UIImage(named: helpScreens[index])
+            
+            self.scrollView.addSubview(imgView)
+        }
+        
+        // 3.
+        scrollView.contentSize = CGSize(width: (scrollView.frame.size.width * CGFloat(helpScreens.count)), height: scrollView.frame.size.height)
+        scrollView.delegate = self
+    }
+    
+    func scrollViewDidEndDecelerating(_ scrollView: UIScrollView) {
+        let pageNumber = scrollView.contentOffset.x / scrollView.frame.size.width
+        pageControl.currentPage = Int(pageNumber)
+    }
+
     
 
 }
